403a42986b03529a36e5deee9035f58db46974c3
Alan Bersia <alanbersia@gmail.com>
2022-12-13 07:37:48 -0300

    Fin sección 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,6 +9,7 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router'
 import { FormsModule } from '@angular/forms'
 import { ComponentsModule } from '../components/components.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { ComponentsModule } from '../components/components.module';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   imports: [ 
     FormsModule,
@@ -30,7 +32,8 @@ import { ComponentsModule } from '../components/components.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ]
 })
 export class PagesModule { }
